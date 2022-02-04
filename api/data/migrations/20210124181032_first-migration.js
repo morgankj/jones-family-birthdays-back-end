fe32fd53e8f08b5f54e9beac1e7f3d2dbe6b208e
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-02-04 14:12:46 -0600

    Added table structure to migration

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,43 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    .createTable("people", (table) => {
+      table.increments("person_id");
+      table.string("person_name").notNullable();
+      table.date("person_birthday").notNullable();
+      table.string("person_image").notNullable();
+      table
+        .integer("person_spouse_id")
+        .unsigned()
+        .references("person_id")
+        .inTable("people")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
     })
-}
+    .createTable("nuclear", (table) => {
+      table.increments("nuclear_id");
+      table.string("nuclear_name").notNullable();
+    })
+    .createTable("person_nuclear", (table) => {
+      table.increments("person_nuclear_id");
+      table
+        .integer("nuclear_id")
+        .unsigned()
+        .notNullable()
+        .references("nuclear_id")
+        .inTable("nuclear")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table
+        .integer("person_id")
+        .unsigned()
+        .notNullable()
+        .references("person_id")
+        .inTable("people")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists("users");
+};
